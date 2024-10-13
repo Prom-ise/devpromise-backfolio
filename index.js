535a24c8b0c797d52d1c944c20c2d24c4542fe73
promise <emmanuelpromise456@gmail.com>
2024-10-13 15:20:55 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Index.js
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
@@ -9,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: 'https://dev-promise.onrender.com',
-  AccessControlAllowOrigin: "*", // Replace this with your frontend URL
+  AccessControlAllowOrigin: "*",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
@@ -17,36 +16,34 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-// Configure Nodemailer
 const transporter = nodemailer.createTransport({
-  service: 'gmail', // Change this to your email service
+  service: 'gmail',
   auth: {
-    user: process.env.EMAIL, // Your email
-    pass: process.env.PASSWORD, // Your email password or app password
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
   },
 });
 
-// Endpoint to handle form submission
 app.post('/send-email', (req, res) => {
-  console.log('Request Body:', req.body); 
-    const { firstName, lastName, email, subject, message } = req.body;
-  
-    const mailOptions = {
-      from: email,
-      to: process.env.EMAIL,
-      subject: subject,
-      text: `Name: ${firstName} ${lastName}\nEmail: ${email}\nMessage: ${message}`,
-    };
-  
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error('Error sending email:', error); // Log the error
-        return res.status(500).send('Error sending email: ' + error.toString());
-      }
-      res.status(200).send('Email sent: ' + info.response);
-    });
+  console.log('Request Body:', req.body);
+  const { firstName, lastName, email, subject, message } = req.body;
+
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL,
+    subject: subject,
+    text: `Name: ${firstName} ${lastName}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      return res.status(500).send('Error sending email: ' + error.toString());
+    }
+    res.status(200).send('Email sent: ' + info.response);
   });
-  
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
